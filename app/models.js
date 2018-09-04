ab2b30e8a9084a660020aa071ae4387af47fc101
davidedelhart <dave@wonderlandlabs.com>
2018-09-04 11:06:58 -0700

    modifying config for Heroku

--- app/models.js
+++ app/models.js
@@ -5,9 +5,13 @@ const customizations = require('../customizations');
 
 const envConfig = config[process.env.NODE_ENV];
 envConfig.logging = false;
-const { username, database, password } = envConfig;
+const { username, database, password, url } = envConfig;
 
-const sequelize = new Sequelize(database, username, password, envConfig);
+let sequelize;
+if (url) {
+  delete envConfig.url;
+  sequelize = new Sequelize(url, envConfig);
+} else { sequelize = new Sequelize(database, username, password, envConfig); }
 
 models.init(sequelize);
 models.datasource = sequelize;
---
+++ config/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  'development': {
+    'username': 'root',
+    'password': null,
+    'database': 'wll_dev',
+    'host': '127.0.0.1',
+    'dialect': 'postgres'
+  },
+  'test': {
+    'username': 'root',
+    'password': null,
+    'database': 'wll_test',
+    'host': '127.0.0.1',
+    'dialect': 'postgres'
+  },
+  'production': {
+    'url': process.env.DATABASE_URL,
+    'protocol': 'postgres',
+    'dialectOptions': {
+      ssl: true
+    },
+    'dialect': 'postgres'
+  }
+};
